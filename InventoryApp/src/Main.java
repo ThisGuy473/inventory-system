6debd8217edee6c8571c85503e9d1cbb08989672
Jeffrey Day <jeffday473@gmail.com>
2023-06-25 10:05:49 -0500

    added all project files

---
+++ InventoryApp/src/Main.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author jeffd
+ */
+public class Main extends Application {
+    
+    @Override
+    public void start(Stage mainStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("MainScreen.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        mainStage.setScene(scene);
+        mainStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
